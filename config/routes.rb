a55f670b4fbc759aacec8579aacd8f0590c2f01d
piyotanpii <chip.n.dale.0301@gmail.com>
2021-04-24 18:34:34 +0900

    crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get '/', to: 'home#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'posts/new', to: 'posts#new'
+  get 'posts/index', to: 'posts#index'
+  post 'posts/create', to: 'posts#create'
+  get 'posts/:id/edit', to: 'posts#edit'
+  patch 'posts/:id/update', to: 'posts#update'
+  delete 'posts/:id/delete', to: 'posts#delete'
+  
 end
